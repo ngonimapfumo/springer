0e7a82a9ef3042c1fc4b3c4bb2d47b3cb2a47714
Ngoni <ngonidzashe.mapfumo@ecocashholdings.co.zw>
2025-03-12 08:43:59 +0200

    Initial commit

---
+++ src/main/kotlin/com/ngoni/springer/SpringerApplication.kt
@@ -0,0 +1,12 @@
+package com.ngoni.springer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringerApplication>(*args)
+
+}
---
+++ src/test/kotlin/com/ngoni/springer/SpringerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ngoni.springer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
