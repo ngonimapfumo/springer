83dbe7b3b9253aeb4ffeacda17bf40e4df37d0f3
Ngoni <ngonidzashe.mapfumo@ecocashholdings.co.zw>
2025-03-12 09:04:35 +0200

    base files

---
+++ src/main/kotlin/com/ngoni/springer/IntegrationTests.kt
@@ -0,0 +1,21 @@
+/*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+import org.testng.annotations.Test
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+
+    */
+/*@Test
+    fun `Assert blog page title, content and status code`() {
+        val entity = restTemplate.getForEntity<String>("/")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<h1>Blog</h1>")
+    }*//*
+
+
+}*/
